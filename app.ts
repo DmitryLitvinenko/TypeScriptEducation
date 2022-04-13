66695e8cb67c8396c5cfc8e377bda42080c96cd6
dmitrijs.litvinenko <dmitrijs.litvinenko@gmail.com>
2022-04-13 16:27:14 +0300

    adding and replacing values in object

--- app.ts
+++ app.ts
@@ -1,9 +1,13 @@
 const person = {
     name: "Dmitry",
     age: 29,
-    hobbies: ['Sports', 'Poker']
+    hobbies: ['Bowling', 'Poker'],
+    role: [2, 'qualification_in']
 };
+
+person.role.push('admin'); //replace value with the same string
+person.role[1] = 10; //adding value to 1 position
 
 for (const hobby of person.hobbies) console.log(hobby);
 
-console.log(person.hobbies)
+console.log(person)
